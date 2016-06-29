779f50fc606a9ebd343735218665c847a2507c96
Jimmi <git@rl-hosting.de>
2016-06-29 23:46:48 +0200

    fixed broken spec

--- spec/services/handle_career_data_spec.rb
+++ spec/services/handle_career_data_spec.rb
@@ -1,24 +1,22 @@
 require "rails_helper"
 
 describe HandleCareerData do
-  let(:diablo_data) { DiabloApiData.perform('eu', 'de_DE', 'Jimmi#2787') }
   context 'new Career' do
-    it { expect {described_class.perform(diablo_data.data)}.to change{Career.count}.from(0).to(1) }
+    it { expect {described_class.perform(DiabloApiData.perform('eu', 'de_DE', 'Jimmi#2787').data)}.to change{Career.count}.from(0).to(1) }
   end
   context 'update Career' do
     it {
       Timecop.freeze(Time.now)
-      described_class.perform(diablo_data.data)
+      described_class.perform(DiabloApiData.perform('eu', 'de_DE', 'Jimmi#2787').data)
       Career.first.update(updated_at: '1-01-01')
-      expect {described_class.perform(diablo_data.data)}.to_not change(Career, :count)
+      expect {described_class.perform(DiabloApiData.perform('eu', 'de_DE', 'Jimmi#2787').data)}.to_not change(Career, :count)
       expect(Career.first.updated_at.utc.to_s).to eq(Time.now.utc.to_s)
       Timecop.return
     }
   end
 
-  let(:diablo_data) { DiabloApiData.perform('eu', 'de_DE', 'abcdefg') }
   context 'wrong battle_tag' do
-    let(:career) { HandleCareerData.perform(diablo_data.data) }
+    let(:career) { HandleCareerData.perform(DiabloApiData.perform('eu', 'de_DE', 'abcdefg').data) }
     it "expect an error code" do
       expect(career).to eq({:code=>"NOTFOUND", :reason=>"The account could not be found."})
     end
